4ee61b60c33c288303e973a86fc66e54d4d3f8d7
MihailKovachev <md.kovachev@protonmail.com>
2026-05-02 11:00:28 +0200

    Project 06 Sketch

---
+++ project06/sketch/sketch.ino
@@ -0,0 +1,36 @@
+int phototransistorValue;
+int phototransistorMin = 1023;
+int phototransistorMax = 0;
+
+const int calibrationLedIndicatorPin = 13; 
+
+void setup() {
+
+  pinMode(calibrationLedIndicatorPin, OUTPUT);
+  digitalWrite(calibrationLedIndicatorPin, HIGH);
+
+  // Calibrate the phototransistor
+
+  while(millis() < 5000)
+  {
+    phototransistorValue = analogRead(A0);
+    if (phototransistorValue > phototransistorMax)
+    {
+      phototransistorMax = phototransistorValue;
+    }
+    if (phototransistorValue < phototransistorMin)
+    {
+      phototransistorMin = phototransistorValue;
+    }
+  }
+
+  digitalWrite(calibrationLedIndicatorPin, LOW);
+}
+
+void loop() {
+  phototransistorValue = analogRead(A0);
+  int pitch = map(phototransistorValue, phototransistorMin, phototransistorMax, 50, 4000);
+  tone(8, pitch, 20);
+  delay(10);
+
+}
